5bf23166d601725379cb418aa3921850e421fbc1
Przemysław Ubuntu <przemyslvw@gmail.com>
2025-02-05 20:18:22 +0100

    npm install react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Items from "./pages/Items";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
-        <div>
-            <h1>CRUD App on AWS</h1>
-            <p>Welcome to our AWS-based CRUD application.</p>
-        </div>
+        <Router>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/items">Items</Link></li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/items" element={<Items />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
     );
 }
 
---
+++ frontend/src/pages/Items.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Items() {
+    return (
+        <div>
+            <h1>Items List</h1>
+            <p>Here will be the list of items.</p>
+        </div>
+    );
+}
+
+export default Items;
