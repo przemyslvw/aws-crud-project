1a2e49648327c7d26c637f377066c99ca2c1dd9f
Przemysław Ubuntu <przemyslvw@gmail.com>
2025-02-05 20:16:13 +0100

    basic react

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>CRUD App on AWS</h1>
+            <p>Welcome to our AWS-based CRUD application.</p>
+        </div>
+    );
+}
+
+export default App;
