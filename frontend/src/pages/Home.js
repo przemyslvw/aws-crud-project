801b383a3fb6726143bb219d11b9b0a42bc9855b
Przemysław Ubuntu <przemyslvw@gmail.com>
2025-02-05 21:51:24 +0100

    homepage update

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Container, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to CRUD App on AWS</h1>
-            <p>This is the home page.</p>
-        </div>
+        <Container sx={{ textAlign: "center", marginTop: 5 }}>
+            <Typography variant="h3" gutterBottom>
+                Rejestr Pracy Kierowców
+            </Typography>
+            <Typography variant="h6" color="textSecondary" paragraph>
+                Aplikacja do ewidencji czasu pracy i przebiegów pojazdów. 
+                Zarejestruj swoją trasę, monitoruj czas pracy i przebieg auta w łatwy sposób!
+            </Typography>
+            <Box sx={{ marginTop: 4 }}>
+                <Button variant="contained" color="primary" component={Link} to="/items">
+                    Przejdź do Rejestru
+                </Button>
+            </Box>
+        </Container>
     );
 }
 
